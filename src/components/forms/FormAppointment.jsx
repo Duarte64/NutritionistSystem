82b3a94a628106cfe7acae51a02d7e39c5819e52
Gabriel Duarte <gabriel.duartepaz@gmail.com>
2021-11-25 23:43:11 -0300

    Add

--- src/components/forms/FormAppointment.jsx
+++ src/components/forms/FormAppointment.jsx
@@ -1,52 +1,45 @@
 import React from 'react'
 import { HalfContainer } from './formStyle';
-import {Button} from '@material-ui/core';
+import {Button, TextField, Autocomplete, Checkbox} from '@material-ui/core';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import { TitleH2 } from '../titles/TitleH2';
-import { TextField } from '@material-ui/core';
 import './materialUI.css';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 
 export function FormAppointment() {
-
 
     return (
         <HalfContainer>
             <TitleH2>Schedule New Medical Appointment</TitleH2>
             <form>
-                <TextField
-                    label="Name" 
-                    color="success"
-                    fullWidth
-                    required 
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={['a','b','c','d','e','c','d']}
+                    renderInput={(params) => <TextField {...params} label="Patient" />}
                 />
+                <FormControlLabel 
+                    control={
+                        <Checkbox 
+                            defaultChecked 
+                            size="medium" 
+                            color="success"
+                        />
+                    }
+                    label="Patient Not Registered Yet"
+                />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>...</LocalizationProvider>
                 <TextField
-                    label="Last Name" 
+                    type="number" 
+                    label="Weight" 
                     variant="outlined" 
                     margin="normal"
-                    color="success"
-                    fullWidth 
-                    required
+                    color="success" 
+                    size = "medium"
+                    required 
                 />
-                <div className="form__spacing">
-                    <TextField
-                        type="number" 
-                        label="Height" 
-                        variant="outlined" 
-                        margin="normal"
-                        color="success"
-                        size="medium" 
-                        required 
-                    />
-                    <TextField
-                        type="number" 
-                        label="Weight" 
-                        variant="outlined" 
-                        margin="normal"
-                        color="success" 
-                        size = "medium"
-                        required 
-                    />
-                </div>
-                <Button type="submit" variant="contained" size="large" color="success" fullWidth>Register</Button>
+            <Button type="submit" variant="contained" size="large" color="success" fullWidth>Register</Button>
             </form>
         </HalfContainer>
     );
